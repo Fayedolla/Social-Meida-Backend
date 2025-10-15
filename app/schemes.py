4308b916cf2722fbd74b072a6dc1cb79f7f8dfd1
Ahmed <fayeda660@gmail.com>
2025-10-15 21:26:02 +0300

    schemes of backend

---
+++ app/schemes.py
@@ -0,0 +1,52 @@
+from datetime import datetime
+from re import I
+from pydantic import BaseModel, EmailStr, conint
+from typing import Optional
+
+
+class PostBase(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+
+class PostCreate(PostBase):
+    pass
+
+class UserOut(BaseModel):
+    email: str
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True
+    
+
+class Post(PostBase):
+    id: int
+    created_at: datetime
+    owner_id: int
+    owner: UserOut
+
+    class Config:
+        from_attributes = True
+
+class UserCreate(BaseModel):
+    email: str
+    password: str
+
+    class Config:
+        from_attributes = True
+
+class UserLogin(UserCreate):
+    pass
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[int] = None
+
+class Vote(BaseModel):
+    post_id: int
+    dir: conint(ge=0, le=1)
