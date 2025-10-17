f865f900b90bf414ffeb4284d487217297a6645b
Ahmed <fayeda660@gmail.com>
2025-10-17 18:13:19 +0300

    CORs

--- app/main.py
+++ app/main.py
@@ -2,8 +2,21 @@ from fastapi import FastAPI, APIRouter
 from .Database import engine
 from .config import settings
 from .Routers import posts, users, votes, auth
+from fastapi.middleware.cors import CORSMiddleware  
 
 app = FastAPI()
+
+origins = ["https://www.google.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],   
+    allow_headers=["*"],
+)
+
+
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(posts.router)
